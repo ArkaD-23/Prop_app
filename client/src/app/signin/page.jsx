78b3648ef69ef4e43ff80b3592f046e399725daa
Arkaprava Dhar <raj2003dhar@gmail.com>
2024-05-24 01:15:17 +0530

    frontend code update

--- client/src/app/signin/page.jsx
+++ client/src/app/signin/page.jsx
@@ -34,89 +34,91 @@ export default function Signin() {
 
   return (
     <div style={{ width: '360px', padding: '8% 0 0', margin: '150px auto' }}>
-      <div
-        style={{
-          position: 'relative',
-          zIndex: '1',
-          background: '#FFFFFF',
-          maxWidth: '360px',
-          margin: '0 auto 100px',
-          padding: '45px',
-          textAlign: 'center',
-          boxShadow: '0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24)',
-        }}
-      >
-        <form>
-          <input
-            id="email"
-            onChange={handleChange}
-            style={{
-              fontFamily: 'Roboto',
-              outline: '0',
-              background: '#f2f2f2',
-              width: '100%',
-              border: '0',
-              margin: '0 0 15px',
-              padding: '15px',
-              boxSizing: 'border-box',
-              fontSize: '14px',
-            }}
-            type="text"
-            placeholder="email"
-          />
-          <input
-            id="password"
-            onChange={handleChange}
-            style={{
-              fontFamily: 'Roboto',
-              outline: '0',
-              background: '#f2f2f2',
-              width: '100%',
-              border: '0',
-              margin: '0 0 15px',
-              padding: '15px',
-              boxSizing: 'border-box',
-              fontSize: '14px',
-            }}
-            type="password"
-            placeholder="password"
-          />
-          <button
-            id="button"
-            onClick={handleSubmit}
-            style={{
-              fontFamily: '"Roboto", sans-serif',
-              textTransform: 'uppercase',
-              outline: '0',
-              background: '#2980b9',
-              width: '100%',
-              border: '0',
-              padding: '15px',
-              color: '#FFFFFF',
-              fontSize: '14px',
-              WebkitTransition: 'all 0.3s ease',
-              transition: 'all 0.3s ease',
-              cursor: 'pointer',
-            }}
-          >
-            login
-          </button>
-          <p
-            style={{
-              margin: '15px 0 0',
-              color: '#b3b3b3',
-              fontSize: '12px',
-            }}
-          >
-            Not registered?{' '}
-            <Link
-              style={{ color: '#2980b9', textDecoration: 'none' }}
-              href="/signup"
+      <div style={{display: 'column',justifyContent:'center',alignContents:'center'}}>
+        <h1>SIGNIN</h1>
+        <div
+          style={{
+            position: 'relative',
+            zIndex: '1',
+            background: '#FFFFFF',
+            maxWidth: '360px',
+            margin: '0 auto 100px',
+            padding: '45px',
+            textAlign: 'center',
+            boxShadow: '0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24)',
+          }}
+        >
+          <form onSubmit={handleSubmit}>
+            <input
+              id="email"
+              onChange={handleChange}
+              style={{
+                fontFamily: 'Roboto',
+                outline: '0',
+                background: '#f2f2f2',
+                width: '100%',
+                border: '0',
+                margin: '0 0 15px',
+                padding: '15px',
+                boxSizing: 'border-box',
+                fontSize: '14px',
+              }}
+              type="text"
+              placeholder="email"
+            />
+            <input
+              id="password"
+              onChange={handleChange}
+              style={{
+                fontFamily: 'Roboto',
+                outline: '0',
+                background: '#f2f2f2',
+                width: '100%',
+                border: '0',
+                margin: '0 0 15px',
+                padding: '15px',
+                boxSizing: 'border-box',
+                fontSize: '14px',
+              }}
+              type="password"
+              placeholder="password"
+            />
+            <button
+              id="button"
+              style={{
+                fontFamily: '"Roboto", sans-serif',
+                textTransform: 'uppercase',
+                outline: '0',
+                background: '#2980b9',
+                width: '100%',
+                border: '0',
+                padding: '15px',
+                color: '#FFFFFF',
+                fontSize: '14px',
+                WebkitTransition: 'all 0.3s ease',
+                transition: 'all 0.3s ease',
+                cursor: 'pointer',
+              }}
             >
-              Create an account
-            </Link>
-          </p>
-        </form>
+              login
+            </button>
+            <p
+              style={{
+                margin: '15px 0 0',
+                color: '#b3b3b3',
+                fontSize: '12px',
+              }}
+            >
+              Not registered?{' '}
+              <Link
+                style={{ color: '#2980b9', textDecoration: 'none' }}
+                href="/signup"
+              >
+                Create an account
+              </Link>
+            </p>
+          </form>
+        </div>
       </div>
     </div>
   );
--- client/src/app/signup/page.jsx
+++ client/src/app/signup/page.jsx
@@ -37,11 +37,11 @@ export default function Signup() {
         position: 'relative', zIndex: '1', background: '#FFFFFF', maxWidth: '360px', margin: '0, auto 100px', padding: '45px', textAlign: 'center',
         boxShadow: '0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24)'
       }}>
-        <form >
+        <form onSubmit={handleSubmit}>
           <input id="username" onChange={handleChange} style={{ fontFamily: 'Roboto', outline: '0', background: '#f2f2f2', width: '100%', border: '0', margin: '0 0 15px', padding: '15px', boxSizing: 'border-box', fontSize: '14px' }} type="text" placeholder="username" />
           <input id="email" onChange={handleChange} style={{ fontFamily: 'Roboto', outline: '0', background: '#f2f2f2', width: '100%', border: '0', margin: '0 0 15px', padding: '15px', boxSizing: 'border-box', fontSize: '14px' }} type="text" placeholder="email" />
           <input id='password' onChange={handleChange} style={{ fontFamily: 'Roboto', outline: '0', background: '#f2f2f2', width: '100%', border: '0', margin: '0 0 15px', padding: '15px', boxSizing: 'border-box', fontSize: '14px' }} type="password" placeholder="password" />
-          <button onClick={handleSubmit} style={{ fontFamily: '"Roboto", sans-serif', textTransform: 'uppercase', outline: '0', background: '#2980b9', width: '100%', border: '0', padding: '15px', color: '#FFFFFF', fontSize: '14px', WebkitTransition: 'all 0.3 ease', transition: 'all 0.3 ease', cursor: 'pointer' }} >Signup</button>
+          <button style={{ fontFamily: '"Roboto", sans-serif', textTransform: 'uppercase', outline: '0', background: '#2980b9', width: '100%', border: '0', padding: '15px', color: '#FFFFFF', fontSize: '14px', WebkitTransition: 'all 0.3 ease', transition: 'all 0.3 ease', cursor: 'pointer' }} >Signup</button>
         </form>
       </div>
     </div>
