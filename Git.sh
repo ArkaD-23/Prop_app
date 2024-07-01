18317828b27e0c9d0982f53669f9215d71260e1c
Arkaprava Dhar <raj2003dhar@gmail.com>
2024-07-01 19:41:11 +0530

    Git script change

--- Git.sh
+++ Git.sh
@@ -4,9 +4,11 @@ commit_message=$1
 
 git add .
 
+echo "Git Status............"
 git status
 
 echo "Commiting message............"
 git commit -m "$commit_message"
 
+echo "Pushing code.............."
 git push origin master
