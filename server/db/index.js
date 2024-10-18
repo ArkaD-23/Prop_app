953c31ea66007f436112b9b93d2250e3f2bbc36d
Arkaprava Dhar <raj2003dhar@gmail.com>
2024-10-19 00:51:13 +0530

    DB connection in separate file

---
+++ server/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(process.env.MONGO_URL)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection FAILED ", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB
--- server/index.js
+++ server/index.js
@@ -1,26 +1,16 @@
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import {app} from "./app.js";
-
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import {app} from './app.js'
 dotenv.config({
-  path:"./.env"
-});
+    path: './.env'
+})
 
-;(async () => {
-  try {
-    mongoose
-      .connect(process.env.MONGO_URL)
-      .then(() => {
-        console.log("Connected to the database........");
-      })
+connectDB()
+.then(() => {
     app.listen(process.env.PORT, () => {
-      console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
-  }
-  catch(err) {
-    console.log(err);
-    process.exit(1);
-  };
-})();
-
-
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
