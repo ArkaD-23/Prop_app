40159eae7c459b150e4199cbd32764bbb9c4f907
Arkaprava Dhar <raj2003dhar@gmail.com>
2025-03-28 14:56:04 +0530

    New monorepo for gateway

--- backend_service/apps/user_service/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { UserServiceModule } from './user_service.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(UserServiceModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
