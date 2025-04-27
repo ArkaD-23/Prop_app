35fa916a34ffb8c4a9caed2c9f2d5e9a60e8c06f
Arkaprava Dhar <raj2003dhar@gmail.com>
2025-04-27 11:37:03 +0530

    Monolith form restored

--- backend/apps/auth_service/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AuthServiceModule } from './auth_service.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AuthServiceModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
